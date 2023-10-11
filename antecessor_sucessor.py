27615784643db730e71d99eea9164f0943ff25b4
janeilson-araujo <janeilsonpaulino013@gmail.com>
2023-10-11 12:56:33 -0300

    atividade04 antecessor e sucessor python

---
+++ antecessor_sucessor.py
@@ -0,0 +1,6 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+print("descubra o antecessor e o sucessor de qualquer numero")
+numero = int(input("digite o numero:"))
+antecessor = numero + 1
+sucessor = numero-1
+print(f"seu antecesso é:{antecessor} , e seu sucessor é:{sucessor}")
